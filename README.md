5d614488adf952144b9fad7c133751a94c0a66be
Aarón Valdez <ldaaron38@gmail.com>
2019-02-24 22:32:29 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+You need to create a .env file with values:
+
+MONGO_USER=yourUser
+MONGO_PASSWORD=yourPassword
+MONGO_DATABASE=yourDatabase
+VOLUME=volume_nyc3_01
---
+++ scripts/deploy_project.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo updating!
+cd ~/calidad-del-aire-cloud
+if [ -d "client" ]; then
+  cd client
+  sudo git pull origin master
+else
+  sudo git clone git@github.com:Kalaston/calidad-del-aire-frontend.git client
+  cd client
+  sudo docker-compose build
+fi
+cd ~/calidad-del-aire-cloud
+if [ -d "api" ]; then
+  cd api
+  sudo git pull origin master
+else
+  sudo git clone git@github.com:Kalaston/calidad-del-aire-api.git api
+  cd api
+  sudo docker-compose build
+fi
+cd ~/calidad-del-aire-cloud
+echo Recreating and starting!
+sudo docker-compose down
+sudo docker-compose -f docker-compose-prod.yml up -d --build --force-recreate
