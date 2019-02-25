0367decdaf2e43441b4a2c90ea73f5b048855823
Aaron Reynoza <ldaaron38@gmail.com>
2019-02-25 08:22:19 -0600

    fix repositories directions on project scripts

--- scripts/deploy_project.sh
+++ scripts/deploy_project.sh
@@ -5,7 +5,7 @@ if [ -d "client" ]; then
   cd client
   sudo git pull origin master
 else
-  sudo git clone git@github.com:Kalaston/calidad-del-aire-frontend.git client
+  sudo git clone git@github.com:utec-investigaciones/calidad-del-aire-client.git client
   cd client
   sudo docker-compose build
 fi
@@ -14,7 +14,7 @@ if [ -d "api" ]; then
   cd api
   sudo git pull origin master
 else
-  sudo git clone git@github.com:Kalaston/calidad-del-aire-api.git api
+  sudo git clone git@github.com:utec-investigaciones/calidad-del-aire-api.git api
   cd api
   sudo docker-compose build
 fi
