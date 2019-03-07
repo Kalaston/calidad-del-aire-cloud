20e75e7b0999b46d393599f76ac863649b078ca8
Aarón Valdez <ldaaron38@gmail.com>
2019-03-06 23:06:15 -0600

    remove sudo from deployment script

--- scripts/deploy_project.sh
+++ scripts/deploy_project.sh
@@ -3,22 +3,22 @@ echo updating!
 cd ~/calidad-del-aire-cloud
 if [ -d "client" ]; then
   cd client
-  sudo git pull origin master
+  git pull origin master
 else
-  sudo git clone git@github.com:utec-investigaciones/calidad-del-aire-client.git client
+  git clone git@github.com:utec-investigaciones/calidad-del-aire-client.git client
   cd client
-  sudo docker-compose build
+  docker-compose build
 fi
 cd ~/calidad-del-aire-cloud
 if [ -d "api" ]; then
   cd api
-  sudo git pull origin master
+  git pull origin master
 else
-  sudo git clone git@github.com:utec-investigaciones/calidad-del-aire-api.git api
+  git clone git@github.com:utec-investigaciones/calidad-del-aire-api.git api
   cd api
-  sudo docker-compose build
+  docker-compose build
 fi
 cd ~/calidad-del-aire-cloud
 echo Recreating and starting!
-sudo docker-compose down
-sudo docker-compose -f docker-compose-prod.yml up -d --build --force-recreate
+docker-compose down
+docker-compose -f docker-compose-prod.yml up -d --build --force-recreate
